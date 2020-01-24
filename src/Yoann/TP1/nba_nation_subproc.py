67c5bb675f7e784710ff1036684b2920979842ec
Yoann067 <yoann.louazel@gmail.com>
2020-01-24 17:30:41 +0100

    I.4 yo

--- src/Yoann/TP1/nba_nation_subproc.py
+++ src/Yoann/TP1/nba_nation_subproc.py
@@ -13,40 +13,37 @@ On affiche juste les infos "prenom nom, club et age"
 '''
 
 from os.path import basename, isfile
-from sys import stdout, stderr, argv
-from datetime import datetime
+from sys import stderr, argv
 
 def traiter_fichier(joueurs):
-    annee_courante = datetime.now().year
-    for un_joueur in joueurs:
-        un_joueur = un_joueur[:-1] # on se débarrasse du '\n' de fin de ligne
-        if un_joueur.endswith(nation):
-            nom, _ , club, annee_nais, _ = un_joueur.split(':')  # conversion de la chaine en tableau
-            age = annee_courante - int(annee_nais)
-            print(nom + ', ' + club + ', ' + str(age))
-    joueurs.close()
-
     # Reouverture du fichier
     # global nom_fichier
     joueurs = open(nom_fichier, 'r')
-
+    chaine = []
     for un_joueur in joueurs:
-        print(un_joueur)
+        chaine += un_joueur
+    joueurs.close()
+    return un_joueur
 
 
-if __name__ == "__main__" :
-    nom_fichier='nba_2019.txt'
-    if len(argv[1:]) == 1 :
-        nation = argv[1]
-    else :
-        stderr.write("Usage : " + basename(argv[0]) + " nation\n")
-        exit(1)
+nom_fichier='nba_2019.txt'
+if len(argv[1:]) == 1 :
+    nation = argv[1]
+else :
+    stderr.write("Usage : " + basename(argv[0]) + " nation\n")
+    exit(1)
 
-    try:
-        joueurs = open(nom_fichier, 'r')
-    except IOError:
-        msg = nom_fichier + ": Pb ouverture !\n"
-        stderr.write(msg)
+try:
+    joueurs = open(nom_fichier, 'r')
+except IOError:
+    msg = nom_fichier + ": Pb ouverture !\n"
+    stderr.write(msg)
+else:
+    tab = traiter_fichier(joueurs)
+    joueurs.close()
+    if __name__ == "__main__" :
+        for ele in tab:
+            print(ele)
     else:
-        traiter_fichier(joueurs)
-        joueurs.close()
+        for ele in tab:
+            print(ele)
