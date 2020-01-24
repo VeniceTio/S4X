59bcda49ff5e16d17eb935ea35571dbd58e2feab
Loïc Gross <loic.gross@unistra.fr>
2020-01-24 17:27:59 +0100

    Début I.4

--- src/Loïc/TP1/nba_nation_parent.py
+++ src/Loïc/TP1/nba_nation_parent.py
@@ -4,4 +4,12 @@
 
 
 import os,sys,subprocess
-pipe = subprocess.Popen(["nba_nation_subproc.py",sys.argv[1]], stdout=subprocess.PIPE, universal_newlines=True)
+pipe = subprocess.Popen(["./nba_nation_subproc.py",sys.argv[1]], stdout=subprocess.PIPE, universal_newlines=True)
+annee_courante = datetime.now().year
+
+ligne = pipe.stdout.readline()
+while ligne:
+    ligne = ligne[:-1]
+    nom, _ , club, annee_nais, _ = un_joueur.split(':')  # conversion de la chaine en tableau
+    age = annee_courante - int(annee_nais)
+    print(nom + ', ' + club + ', ' + str(age))
--- src/Loïc/TP1/nba_nation_subproc.py
+++ src/Loïc/TP1/nba_nation_subproc.py
@@ -17,36 +17,28 @@ from sys import stdout, stderr, argv
 from datetime import datetime
 
 def traiter_fichier(joueurs):
-    annee_courante = datetime.now().year
+    res = []
     for un_joueur in joueurs:
         un_joueur = un_joueur[:-1] # on se débarrasse du '\n' de fin de ligne
         if un_joueur.endswith(nation):
-            nom, _ , club, annee_nais, _ = un_joueur.split(':')  # conversion de la chaine en tableau
-            age = annee_courante - int(annee_nais)
-            print(nom + ', ' + club + ', ' + str(age))
+            res += [un_joueur]
     joueurs.close()
+    return res
 
-    # Reouverture du fichier
-    # global nom_fichier
+
+nom_fichier='nba_2019.txt'
+if len(argv[1:]) == 1 :
+    nation = argv[1]
+else:
+    stderr.write("Usage : " + basename(argv[0]) + " nation\n")
+    exit(1)
+try:
     joueurs = open(nom_fichier, 'r')
-
-    for un_joueur in joueurs:
-        print(un_joueur)
-
-
-if __name__ == "__main__" :
-    nom_fichier='nba_2019.txt'
-    if len(argv[1:]) == 1 :
-        nation = argv[1]
-    else :
-        stderr.write("Usage : " + basename(argv[0]) + " nation\n")
-        exit(1)
-
-    try:
-        joueurs = open(nom_fichier, 'r')
-    except IOError:
-        msg = nom_fichier + ": Pb ouverture !\n"
-        stderr.write(msg)
-    else:
-        traiter_fichier(joueurs)
-        joueurs.close()
+except IOError:
+    msg = nom_fichier + ": Pb ouverture !\n"
+    stderr.write(msg)
+else:
+    tab = traiter_fichier(joueurs)
+    joueurs.close()
+    for ele in tab:
+        print(ele)
