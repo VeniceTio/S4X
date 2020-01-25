14e755a4d3daf7dab5067d93ff6bb2c900e5694b
Loïc Gross <loic.gross@unistra.fr>
2020-01-25 20:50:34 +0100

    TP1 Fin I.4

--- src/Loïc/TP1/nba_nation_parent.py
+++ src/Loïc/TP1/nba_nation_parent.py
@@ -4,12 +4,13 @@
 
 
 import os,sys,subprocess
+from datetime import datetime
 pipe = subprocess.Popen(["./nba_nation_subproc.py",sys.argv[1]], stdout=subprocess.PIPE, universal_newlines=True)
 annee_courante = datetime.now().year
 
 ligne = pipe.stdout.readline()
 while ligne:
-    ligne = ligne[:-1]
-    nom, _ , club, annee_nais, _ = un_joueur.split(':')  # conversion de la chaine en tableau
+    nom, _ , club, annee_nais, _ = ligne.split(':')  # conversion de la chaine en tableau
     age = annee_courante - int(annee_nais)
     print(nom + ', ' + club + ', ' + str(age))
+    ligne = pipe.stdout.readline()
