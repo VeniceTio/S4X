7b0c15ca2a8132d5b647ed2ca307d973c2fb6938
Yoann067 <yoann.louazel@gmail.com>
2020-02-04 14:41:16 +0100

    nba_nation_thread

---
+++ src/Yoann/TP2/nba_par_nation_thread.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python3
+# -*- coding: utf8 -*-
+
+from threading import Thread
+from time import sleep
+from sys import argv, stderr, stdout
+from queue import Queue
+from datetime import datetime
+from os.path import basename
+
+
+# définition de la fonction writer
+def producteur(fifo, joueurs, nation):
+    for un_joueur in joueurs:
+        un_joueur = un_joueur[:-1]
+        if un_joueur.endswith(nation):
+            fifo.put(un_joueur)
+            sleep(2)
+    fifo.task_done()
+    joueurs.close()
+
+# définition de la fonction reader
+def consommateur(fifo):
+     annee_courante = datetime.now().year
+     un_joueur = fifo.get() # get bloquant # idem que: un_joueur = fifo.get(block=True)
+     while un_joueur is not None: # None: valeur spéciale ‘sentinelle’ prévue par le langage
+         nom, _, club, annee_nais, _ = un_joueur.split(':')  # conversion de la chaine en tableau
+         age = annee_courante - int(annee_nais)
+         print(nom + ', ' + club + ', ' + str(age))
+         un_joueur = fifo.get() # get bloquant
+
+ # main
+ # lecture/contrôles des paramètres, ouverture du fichier
+nom_fichier = 'nba_2019.txt'
+if len(argv[1:]) != 0:
+    nation = argv[1]
+else:
+    stderr.write("Usage : " + basename(argv[0]) + " nation\n")
+    exit(1)
+
+try:
+    joueurs = open(nom_fichier, 'r')
+except IOError:
+    msg = nom_fichier + ": Pb ouverture !\n"
+    stderr.write(msg)
+else:
+    # création de la file FIFO (Queue)
+    Fifo = Queue()
+    # Instanciation et démarrage des deux threads
+    thread1 = Thread(target=producteur, args=(Fifo,joueurs,nation))
+    thread2 = Thread(target=consommateur, args=(Fifo,))
+    thread1.start()
+    thread2.start()
+    thread1.join()
+    thread2.join()
+    # fermeture du fichier
+    joueurs.close()
