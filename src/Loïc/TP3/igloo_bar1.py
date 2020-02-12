27cdb52a690318150e084bd2f6b49bd99cc784dd
Loïc Gross <loic.gross@unistra.fr>
2020-02-12 15:22:59 +0100

    TP3 debut partie 2

---
+++ src/Loïc/TP3/igloo_bar1.py
@@ -0,0 +1,98 @@
+#! /usr/bin/python3
+# -*- coding: UTF-8 -*-
+# igloo_bar1.py
+# +++++++++++++++++++++++++
+# Squelette à compléter !
+# +++++++++++++++++++++++++
+
+# Contrainte : Nbre de clients en train de  boire <= nbre de sièges ds le bar
+
+
+from threading import Thread, Semaphore, BoundedSemaphore
+from queue import Queue
+from time import sleep
+from random import random, uniform
+
+# __ var. globales __
+# nb : Seats doit être connu avant de pouvoir initialiser la variable de
+#      classe <enter_multiplex>
+
+DefSeatsValue = 5 # valeur par défaut
+try:
+    Seats = input('Nbre de places ? (5 par defaut: taper Entree) ')
+    if Seats == "":
+        Seats = DefSeatsValue
+    else:
+        Seats = int(Seats)
+    Nb_clients = int(input('Nbre de clients ? '))
+    assert(Seats > 0)
+    assert(Nb_clients > 0)
+except ValueError:
+    print('Nombre entier requis !')
+    exit(1)
+except AssertionError:
+    print('Nbre (places et clients) positif requis !')
+    exit(1)
+
+class Client(Thread):
+    # variables de classe
+    drinking = 0
+    drink_mutex = Semaphore(1) # pour protéger les maj du compteur <drinking>
+    enter_multiplex = BoundedSemaphore(Seats) # pour respecter la contrainte
+
+    def __init__(self, id, fifo):
+        super().__init__()  # Thread.__init__(self)
+        self._id = id
+        self._fifo = fifo
+
+    def run(self):
+        self.to_arrive() # pour "espacer" les arrivees
+        Client.enter_multiplex.acquire()    # "zone" où il ne doit pas y avoir plus <Seats> consommateurs
+        #...
+        Client.drinking += 1
+        state = "s'installe et boit"
+        item = "client {0:3d} {2:30s} compteur = {1:d}".format(self._id, \
+                                                 Client.drinking, state)
+        self._fifo.put(item)
+        Client.drink_mutex.release()
+
+        # phase de consommation
+        self.drink()
+
+        # Sortie d'un client
+        Client.drink_mutex.acquire()
+        Client.drinking -= 1
+        state = "quitte sa place"
+        item = "client {0:3d} {2:30s} compteur = {1:d}".format(self._id, \
+                                                 Client.drinking, state)
+        self._fifo.put(item)
+        Client.enter_multiplex.release()
+
+    def drink(self):
+        sleep(uniform(1, 3))
+
+    def to_arrive(self):
+        #sleep(uniform(0, 20))
+        sleep(uniform(0, Nb_clients // 2))
+        item = 'client {:3d} arrive au bar'.format(self._id)
+        self._fifo.put(item)
+
+class Affiche(Thread):
+    def __init__(self, fifo):
+        super().__init__()  # Thread.__init__(self)
+        self.stop_and_join()
+
+    def run(self):
+        print(Fifo.get())
+
+    def stop_and_join(self):
+        self.start()
+        self.join()
+
+# main
+Fifo = Queue()
+affi = Affiche(Fifo)
+Th_clients = [Client(i+1, Fifo) for i in range(Nb_clients)]
+for t in Th_clients : t.start()
+for t in Th_clients : t.join()
+print("\nPlus de client !")
--- src/Loïc/TP3/ping_pong_sem1.py
+++ src/Loïc/TP3/ping_pong_sem1.py
@@ -15,42 +15,36 @@ from time import sleep
 from random import random
 import sys
 from threading import Thread, Semaphore
+from queue import Queue
 # import A COMPLETER
 
 # Pour compter le nombre de ligne : | wc -l
 
-class Ping():
-    def __init__(self):
-        self.t = Thread(target=self.run)
-    def start(self):
-        self.t.start()
+class Affiche(Thread):
     def run(self):
-        sleep(random()) # pour un ordre d'entree ds la boucle aleatoire
-                        # (en fct du thread)
-        # A COMPLETER
-        bSema.acquire()
-        print("ping ... ", end="")
-        bSema2.release()
-    def join(self):
-        self.t.join()
+        item = Fifo.get()   # get bloquant
+        while not (item is None):   # while item is not None
+            print(item, end="")
+            sleep(0.001)  # courte pause (vous pouvez augmenter la durée si ça vous détend ;-)
+            item = Fifo.get() # get bloquant
+
+def ping() :
+    sleep(random()) # pour un ordre d'entree ds la boucle aleatoire
+                    # (en fct du thread)
+    # A COMPLETER
+    bSema.acquire()
+    Fifo.put("ping...", block = True)
+    bSema2.release()
 
 
-class Pong():
-    def __init__(self):
-        self.t = Thread(target=self.run)
-    def start(self):
-        self.t.start()
-    def run(self):
-        sleep(random()) # pour un ordre d'entree ds la boucle aleatoire
-                        # (en fct du thread)
-        # A COMPLETER
-        bSema2.acquire()
-        print("pong")
-        bSema.release()
-    def join(self):
-        self.t.join()
-
-
+def pong() :
+    # global bSema  # pas obligatoire
+    sleep(random()) # pour un ordre d'entree ds la boucle aleatoire
+                    # (en fct du thread)
+    # A COMPLETER
+    bSema2.acquire()
+    Fifo.put("pong\n", block = True)
+    bSema.release()
 
 
 # main thread
@@ -60,18 +54,22 @@ if __name__ == '__main__' :
     else :
         N = 100 	# 100 ping pong par defaut
 
+    Fifo = Queue()
+    affi = Affiche()
     bSema = Semaphore(1)
     bSema2 = Semaphore(0)
-
     # creation d'une liste de N threads ping ; chaque thread fait UN (seul) ping
-    Threads_ping = [ Ping() for i in range(N) ]
+    Threads_ping = [ Thread(target=ping) for i in range(N) ]
     # creation d'une liste de N threads pong ; chaque thread fait UN (seul) pong
-    Threads_pong = [ Pong() for i in range(N) ]
+    Threads_pong = [ Thread(target=pong) for i in range(N) ]
 
+    affi.start()
     for t in Threads_ping : t.start()
     for t in Threads_pong : t.start()
     for t in Threads_ping : t.join()
     for t in Threads_pong : t.join()
+    Fifo.put(None);
+    affi.join()
 
     print("\nFin de partie !")
 
